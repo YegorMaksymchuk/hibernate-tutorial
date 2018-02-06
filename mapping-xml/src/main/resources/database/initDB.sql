6f7b28d8b677ee71b2ba71aef088d2e1a4e53ddf
yemax <egormaksimchuk@gmail.com>
2018-02-06 14:37:43 +0200

    change in db layer.

--- mapping-xml/src/main/resources/database/initDB.sql
+++ mapping-xml/src/main/resources/database/initDB.sql
@@ -1,27 +1,33 @@
 CREATE SCHEMA hibernate DEFAULT CHARACTER SET utf8mb4;
 
+CREATE TABLE IF NOT EXISTS hibernate.projects (
+  id           BIGINT PRIMARY KEY AUTO_INCREMENT,
+  name         VARCHAR(100) NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS hibernate.developers (
   id         BIGINT PRIMARY KEY AUTO_INCREMENT,
   first_name VARCHAR(50)  NOT NULL,
   last_name  VARCHAR(100) NOT NULL,
   specialty  VARCHAR(100) NOT NULL,
-  salary     DECIMAL      NOT NULL
+  salary     DECIMAL      NOT NULL,
+  project_id BIGINT ,
+  FOREIGN KEY (project_id) REFERENCES projects (id)
 );
 
-CREATE TABLE IF NOT EXISTS hibernate.projects (
-  id           BIGINT PRIMARY KEY AUTO_INCREMENT,
-  name         VARCHAR(100) NOT NULL,
-  developer_id BIGINT ,
-  FOREIGN KEY (developer_id) REFERENCES developers (id)
-);
-
---DROP TABLE hibernate.skills
-
-/*
 CREATE TABLE IF NOT EXISTS hibernate.skills (
   id           BIGINT PRIMARY KEY AUTO_INCREMENT,
   skill        VARCHAR(100) NOT NULL,
-  developer_id BIGINT ,
-  FOREIGN KEY (developer_id) REFERENCES developers (id)
+  skill_idx    INT(50) NOT NULL
 );
-*/
+
+CREATE TABLE IF NOT EXISTS hibernate.developer_skills(
+id BIGINT PRIMARY KEY AUTO_INCREMENT,
+skill_id BIGINT NOT NULL,
+developer_id BIGINT NOT NULL,
+  FOREIGN KEY (developer_id) REFERENCES developers (id),
+  FOREIGN KEY (skill_id) REFERENCES skills (id)
+);
+
+DROP TABLE hibernate.skills
+DROP SCHEMA hibernate;
--- mapping-xml/src/main/resources/database/populateDB.sql
+++ mapping-xml/src/main/resources/database/populateDB.sql
@@ -1,20 +1,15 @@
--- developers
-INSERT INTO hibernate.developers (first_name, last_name , specialty, salary) VALUES ( 'Ivan', 'Ivanon', 'Java Developer', 3000.00);
-INSERT INTO hibernate.developers (first_name, last_name , specialty, salary) VALUES ( 'Kolya', 'Nikolaev', 'Java  Developer', 4000.00);
-INSERT INTO hibernate.developers (first_name, last_name , specialty, salary) VALUES ( 'Mykola', 'Petrov', 'Java  Developer', 4000.00);
-INSERT INTO hibernate.developers (first_name, last_name , specialty, salary) VALUES ( 'Anna', 'Nikolaeva', 'Java Script Developer', 2000.00);
-INSERT INTO hibernate.developers (first_name, last_name , specialty, salary) VALUES ( 'Tatjana', 'Maksimova', 'Java Script Developer', 2000.00);
-INSERT INTO hibernate.developers (first_name, last_name , specialty, salary) VALUES ( 'Evgenija', 'Galevich', 'Java Script Developer', 2000.00);
-
 --Projects
-INSERT INTO hibernate.projects (name, developer_id) VALUES('Hibernate',1);
-INSERT INTO hibernate.projects (name, developer_id) VALUES('Hibernate',2);
-INSERT INTO hibernate.projects (name, developer_id) VALUES('WildFly',3);
-INSERT INTO hibernate.projects (name, developer_id) VALUES('AngularJS4',4);
-INSERT INTO hibernate.projects (name, developer_id) VALUES('AngularJS5',5);
-INSERT INTO hibernate.projects (name, developer_id) VALUES('AngularJS5.1',6);
+INSERT INTO hibernate.projects (name) VALUES('Hibernate');
+INSERT INTO hibernate.projects (name) VALUES('WildFly');
+INSERT INTO hibernate.projects (name) VALUES('AngularJS');
 
-
+-- developers
+INSERT INTO hibernate.developers (first_name, last_name , specialty, salary, project_id) VALUES ( 'Ivan', 'Ivanon', 'Java Developer', 3000.00, 1);
+INSERT INTO hibernate.developers (first_name, last_name , specialty, salary, project_id) VALUES ( 'Kolya', 'Nikolaev', 'Java  Developer', 4000.00, 1);
+INSERT INTO hibernate.developers (first_name, last_name , specialty, salary, project_id) VALUES ( 'Mykola', 'Petrov', 'Java  Developer', 4000.00, 2);
+INSERT INTO hibernate.developers (first_name, last_name , specialty, salary, project_id) VALUES ( 'Anna', 'Nikolaeva', 'Java Script Developer', 2000.00, 3);
+INSERT INTO hibernate.developers (first_name, last_name , specialty, salary, project_id) VALUES ( 'Tatjana', 'Maksimova', 'Java Script Developer', 2000.00, 3);
+INSERT INTO hibernate.developers (first_name, last_name , specialty, salary, project_id) VALUES ( 'Evgenija', 'Galevich', 'Java Script Developer', 2000.00, 3);
 
 SELECT * FROM hibernate.developers;
 SELECT * FROM hibernate.projects;
@@ -23,4 +18,4 @@ SELECT hibernate.projects.name ,count(hibernate.developers.id)
 FROM hibernate.developers
 INNER JOIN hibernate.projects
 ON developers.id=projects.developer_id
-group by projects.name;
+GROUP BY projects.name;
