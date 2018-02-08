42af81e75a507aad5c023200e9b33dfa2c1c1f1a
yemax <egormaksimchuk@gmail.com>
2018-02-08 12:17:07 +0200

    add new class

--- mapping-xml/src/main/java/com/company/hibernate/HibernateDemo.java
+++ mapping-xml/src/main/java/com/company/hibernate/HibernateDemo.java
@@ -1,19 +1,12 @@
 package com.company.hibernate;
 
 import com.company.hibernate.dao.DeveloperDAO;
-import com.company.hibernate.dao.ProjectDAO;
-import com.company.hibernate.dao.SkillDAO;
 import com.company.hibernate.dao.impl.DeveloperDAOImpl;
-import com.company.hibernate.dao.impl.ProjectDAOImpl;
-import com.company.hibernate.dao.impl.SkillDAOImpl;
 import com.company.hibernate.model.Developer;
-import com.company.hibernate.model.Project;
-import com.company.hibernate.model.Skill;
 import com.company.hibernate.services.DeveloperService;
 
 import java.math.BigDecimal;
 import java.util.List;
-import java.util.logging.Logger;
 
 public class HibernateDemo {
     public static void main(String[] args) {
@@ -21,8 +14,8 @@ public class HibernateDemo {
 
         DeveloperDAO developerDAO = new DeveloperDAOImpl();
 
-//        DeveloperService developerService = new DeveloperService();
-//        developerService.addJavaDev("Alex", "Nazarchuk", BigDecimal.valueOf(1000.00));
+        DeveloperService developerService = new DeveloperService();
+        developerService.addJavaDev("Alex", "Nazarchuk", BigDecimal.valueOf(1000.00));
 
         printer(developerDAO.getAllDeveloperSQL());
         System.exit(0);
